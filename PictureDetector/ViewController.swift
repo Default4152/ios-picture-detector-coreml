7c6d05dc5b78ed07bc23d457f2c7321f9381888c
Conner Alegre <cga0@pm.me>
2018-08-06 17:47:16 -0600

    implement basic image ML for detection

--- PictureDetector/ViewController.swift
+++ PictureDetector/ViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var imageView: UIImageView!
     @IBOutlet var tableView: UITableView!
+    var resnet50Model = Resnet50()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        if let image = imageView.image {
+            whatIsThis(image: image)
+        }
+    }
+    
+    func whatIsThis(image: UIImage) {
+        if let model = try? VNCoreMLModel(for: resnet50Model.model) {
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                if let results = request.results as? [VNClassificationObservation] {
+                    print(results)
+                }
+            }
+            if let imageData = image.jpegData(compressionQuality: 1.0) {
+                let handler = VNImageRequestHandler(data: imageData, options: [:])
+                try? handler.perform([request])
+            }
+        }
+        
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
